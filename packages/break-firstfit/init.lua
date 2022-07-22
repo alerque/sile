54f45cf0c7a769a3c76fda1086388d4a5b390513
Caleb Maclennan <caleb@alerque.com>
2022-07-22 14:46:46 +0300

    refactor(typesetter): Move alternative typesetter function to class

--- packages/break-firstfit/init.lua
+++ packages/break-firstfit/init.lua
@@ -7,37 +7,13 @@ package._name = "break-firstfit"
 -- algorithm, especially when you're dealing with vertical
 -- typesetting. Oh, and it's really fast too.
 
-local firstfit = function (typesetter, nl, breakWidth)
-  local breaks = {}
-  local length = SILE.length()
-  for i = 1,#nl do local n = nl[i]
-    if n.is_box then
-      SU.debug("break", n .. " " .. tostring(n:lineContribution()))
-      length = length + n:lineContribution()
-      SU.debug("break", " Length now " .. tostring(length) .. " breakwidth ".. tostring(breakWidth))
-    end
-    if not n.is_box or n.isHangable then
-      SU.debug("break", n )
-      if n.is_glue then
-        length = length + n.width:absolute()
-      end
-      SU.debug("break", " Length now " .. tostring(length) .. " breakwidth " .. tostring(breakWidth))
-      -- Can we break?
-      if length:tonumber() >= breakWidth:tonumber() then
-        SU.debug("break", "Breaking!")
-        breaks[#breaks+1] = { position = i, width = breakWidth}
-        length = SILE.length()
-      end
-    end
-  end
-  breaks[#breaks+1] = { position = #nl, width = breakWidth}
-  return typesetter:breakpointsToLines(breaks)
-end
-
 function package:_init (class)
 
   base._init(self, class)
 
+  SILE.require("typesetters.firstfit")
+
+  SILE.typesetter = SILE.typesetters.firstfit()
   -- exports
   SILE.typesetter._breakIntoLines_firstfit = firstfit
 
