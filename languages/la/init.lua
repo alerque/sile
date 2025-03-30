29e4968b183b942e80d4d9055f52c8b262be2aa7
Caleb Maclennan <caleb@alerque.com>
2025-03-31 00:08:15 +0300

    latinificate

--- languages/la/init.lua
+++ languages/la/init.lua
@@ -1,2 +1,9 @@
-local hyphens = require("languages.la.hyphens-tex")
-SILE.hyphenator.languages["la"] = hyphens
+local unicode = require("languages.unicode")
+
+local language = pl.class(unicode)
+language._name = "la"
+
+-- local hyphens = require("languages.la.hyphens-tex")
+-- SILE.hyphenator.languages["la"] = hyphens
+
+return language
