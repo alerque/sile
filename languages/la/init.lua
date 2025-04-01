05f98873703cdf2fdc5fd31c44cedcfabc1f6289
Caleb Maclennan <caleb@alerque.com>
2025-04-01 22:05:08 +0300

    fixup! latinificate

--- languages/la/init.lua
+++ languages/la/init.lua
@@ -3,7 +3,4 @@ local unicode = require("languages.unicode")
 local language = pl.class(unicode)
 language._name = "la"
 
--- local hyphens = require("languages.la.hyphens-tex")
--- SILE.hyphenator.languages["la"] = hyphens
-
 return language
