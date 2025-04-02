ddf3e23469626b9943a2c176b9bac00d00b34f1d
Caleb Maclennan <caleb@alerque.com>
2025-04-02 19:08:33 +0300

    fixup! walk back the defaultScript hackery

--- languages/kn/init.lua
+++ languages/kn/init.lua
@@ -3,7 +3,7 @@ local unicode = require("languages.unicode")
 local language = pl.class(unicode)
 language._name = "kn"
 
-function language:declareSettings (typesetter)
+function language.declareSettings (_)
    -- TODO get this *unset* when switching to other languages
    SILE.settings:set("font.script", "Knda")
 end
