ee57d638e8546dde4681f7b93185bc29f647cf76
Caleb Maclennan <caleb@alerque.com>
2025-04-02 17:47:44 +0300

    onboard fr

---
+++ languages/ca/nodemaker.lua
@@ -0,0 +1,12 @@
+local unicode = require("languages.unicode-nodemaker")
+
+local nodeMaker = pl.class(unicode)
+nodeMaker._name = "ca"
+
+-- TODO find a more ergonomic place to put obvious properties
+function nodeMaker:_init (language, options)
+   unicode._init(self, language, options)
+   self.quoteTypes = { qu = true } -- split tokens at apostrophes etc.
+end
+
+return nodeMaker
