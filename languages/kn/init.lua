72adfd8e69ea6629a417583f12c20784520e5084
Caleb Maclennan <caleb@alerque.com>
2025-04-02 18:58:53 +0300

    fixup! walk back the defaultScript hackery

--- languages/kn/init.lua
+++ languages/kn/init.lua
@@ -1,6 +1,6 @@
 local unicode = require("languages.unicode")
 
-local language = pl.class(base)
+local language = pl.class(unicode)
 language._name = "kn"
 
 function language:declareSettings (typesetter)
