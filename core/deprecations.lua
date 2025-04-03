79c7fcfe1ce04d31a2e395c1cd5c4f931c8c2a47
Caleb Maclennan <caleb@alerque.com>
2025-04-03 19:03:03 +0300

    onboard hy

--- core/deprecations.lua
+++ core/deprecations.lua
@@ -256,6 +256,11 @@ setmetatable(SILE.nodeMakers, {
    __index = nonodemakers,
 })
 
+SILE.tokenizers = {}
+setmetatable(SILE.tokenizers, {
+   __index = nonodemakers,
+})
+
 
 -- luacheck: ignore updatePackage
 -- luacheck: ignore installPackage
