fa2ab0a8e519cfa69594cd56ecea769e252bcead
Caleb Maclennan <caleb@alerque.com>
2025-04-10 00:30:25 +0300

    aug

--- shapers/pango.lua
+++ shapers/pango.lua
@@ -6,6 +6,9 @@ require("string")
 local pango = lgi.Pango
 local fm = lgi.PangoCairo.FontMap.get_default()
 local pango_context = lgi.Pango.FontMap.create_context(fm)
+pango_context:set_round_glyph_positions(false)  -- We want exact positions
+pango_context:set_language(pango.Language.get_default())
+pango_context:set_base_dir(pango.Direction.LTR)  -- Default to left-to-right
 
 local base = require("shapers.base")
 
@@ -15,8 +18,40 @@ local function _shape (text, item)
    local offset = item.offset
    local length = item.length
    local analysis = item.analysis
+
+   SU.debug("pango", "Shape input:", {
+      text = text,
+      offset = offset,
+      length = length,
+      has_analysis = analysis ~= nil,
+      analysis_font = analysis and analysis.font or "no font",
+      analysis_lang = analysis and analysis.language or "no language"
+   })
+
+   local shaped_text = string.sub(text, 1 + offset, 1 + offset + length)
+   SU.debug("pango", "Shaping text segment:", shaped_text)
+
    local pgs = pango.GlyphString.new()
-   pango.shape(string.sub(text, 1 + offset), length, analysis, pgs)
+   -- Make sure we're passing the font from the analysis
+   if analysis and analysis.font then
+      pango.shape(shaped_text, -1, analysis, pgs)
+   else
+      -- Try fallback if no font in analysis
+      local desc = pango.FontDescription.new()
+      desc:set_family("serif") -- fallback font
+      desc:set_size(12 * pango.SCALE)
+      local font = pango_context:load_font(desc)
+      analysis = pango.Analysis.new()
+      analysis.font = font
+      pango.shape(shaped_text, -1, analysis, pgs)
+   end
+
+   SU.debug("pango", "Shaped result:", {
+      num_glyphs = pgs.num_glyphs,
+      has_glyphs = pgs.glyphs ~= nil,
+      glyphs_table = pgs.glyphs and pl.pretty.write(pgs.glyphs) or "no glyphs"
+   })
+
    return pgs
 end
 
@@ -25,75 +60,82 @@ shaper._name = "pango"
 
 -- TODO: refactor so method accepts self
 function shaper.getFace (options)
-   local pal
-   if options.pal then
-      return options.pal
-   end
-   local p = pl.pretty.write(options, "")
-   if palcache[p] then
-      return palcache[p]
+   SU.debug("pango", "Getting face for options:", pl.pretty.write(options))
+
+   local pal = pango.AttrList.new()
+
+   -- Create a font description
+   local desc = pango.FontDescription.new()
+
+   if options.family then
+      desc:set_family(options.family)
    else
-      pal = pango.AttrList.new()
-      if options.language then
-         pal:insert(pango.Attribute.language_new(pango.Language.from_string(options.language)))
-      end
-      if options.font then
-         pal:insert(pango.Attribute.family_new(options.font))
-      end
-      if options.weight then
-         pal:insert(pango.Attribute.weight_new(tonumber(options.weight)))
-      end
-      if options.size then
-         pal:insert(pango.Attribute.size_new(options.size * 1024 * 0.75))
-      end -- I don't know why 0.75
-      if options.style then
-         pal:insert(
-            pango.Attribute.style_new(
-               options.style:lower() == "italic" and pango.Style.ITALIC or pango.Style.NORMAL
-            )
-         )
-      end
-      if options.variant then
-         pal:insert(
-            pango.Attribute.variant_new(
-               options.variant:lower() == "smallcaps" and pango.Variant.SMALL_CAPS or pango.Variant.NORMAL
-            )
-         )
-      end
+      desc:set_family("serif") -- fallback
    end
+
+   if options.weight then
+      desc:set_weight(tonumber(options.weight))
+   end
+
+   if options.size then
+      desc:set_size(options.size * pango.SCALE)
+   end
+
+   -- Add the font description to the attribute list
+   pal:insert(pango.attr_font_desc_new(desc))
+
    if options.language then
-      pango_context:set_language(pango.Language.from_string(options.language))
+      pal:insert(pango.attr_language_new(pango.Language.from_string(options.language)))
    end
-   palcache[p] = pal
+
+   SU.debug("pango", "Created attribute list with font description:", desc:to_string())
+
    return pal
 end
 
 function shaper:shapeToken (text, options)
+   SU.debug("pango", "Shaping text:", text, "with options:", pl.pretty.write(options))
+
    local pal = SILE.font.cache(options, self.getFace)
-   local rv = {}
+   SU.debug("pango", "Got PAL:", pal)
+
    local items = pango.itemize(pango_context, text, 0, string.len(text), pal, nil)
+   SU.debug("pango", "Itemization result count:", #items)
+
+   local rv = {}
    local twidth = SILE.types.length()
+
    for i = 1, #items do
       local item = items[i]
       local pgs = _shape(text, item)
-      -- local text = string.sub(text,1+items[i].offset, items[i].length)
-      -- local depth, height = 0,0
-      local font = items[i].analysis.font
-      twidth = twidth + pgs:get_width() / 1024
-      for g in pairs(pgs.glyphs) do
-         local rect = font:get_glyph_extents(pgs.glyphs[g].glyph)
-         table.insert(rv, {
-            height = -rect.y / 1024,
-            depth = (rect.y + rect.height) / 1024,
-            width = rect.width / 1024,
-            glyph = pgs.glyphs[g].glyph,
-            pgs = pgs,
-            font = font,
-            -- text = text
-         })
+      local font = item.analysis.font
+
+      if not font then
+         SU.debug("pango", "No font in analysis for item:", i)
+         goto continue
       end
+
+      SU.debug("pango", "Processing glyphs for item:", i)
+      for g = 0, (pgs.num_glyphs or 0) - 1 do
+         local glyph = pgs.glyphs[g]
+         if glyph then
+            local rect = font:get_glyph_extents(glyph.glyph)
+            table.insert(rv, {
+               height = -rect.y / 1024,
+               depth = (rect.y + rect.height) / 1024,
+               width = rect.width / 1024,
+               glyph = glyph.glyph,
+               pgs = pgs,
+               font = font,
+            })
+         end
+      end
+
+      ::continue::
    end
-   return rv, twidth
+
+   SU.debug("pango", "Final shaped result:", pl.pretty.write(rv))
+   return rv
 end
 
 function shaper:addShapedGlyphToNnodeValue (nnodevalue, shapedglyph)
