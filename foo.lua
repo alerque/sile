1ab1f7ac1949a73de404cee198c580f617a6ca2c
Caleb Maclennan <caleb@alerque.com>
2025-04-10 15:43:49 +0300

    g4o

--- foo.lua
+++ foo.lua
@@ -2,27 +2,33 @@ local lgi = require 'lgi'
 local Pango = lgi.Pango
 local PangoCairo = lgi.PangoCairo
 
+-- Use a known-good font and create a context
 local font_map = PangoCairo.FontMap.get_default()
 local context = font_map:create_context()
-context:set_language(Pango.Language.from_string("en-us"))
+local font_desc = Pango.FontDescription.from_string("DejaVu Sans 12")
+local font = context:load_font(font_desc)
+assert(font, "Font failed to load")
 
-local font_desc = Pango.FontDescription.new()
-font_desc:set_family("Serif")
-font_desc:set_size(12 * Pango.SCALE)
-
+-- Text and length
 local text = "text"
+local bytes = text:len()
 
--- Create a valid analysis object
-local analysis = Pango.Analysis()
-analysis.font = context:load_font(font_desc)
-analysis.level = 0
+-- Build a fully populated Pango.Analysis
+local analysis = Pango.Analysis {
+  font = font,
+  level = 0,
+  gravity = Pango.Gravity.SOUTH,
+  flags = 0,
+  script = Pango.Script.LATIN
+}
 
-local pgs = Pango.GlyphString.new()
-Pango.shape(text, #text, analysis, pgs)
+-- Shape
+local glyphs = Pango.GlyphString.new()
+Pango.shape(text, bytes, analysis, glyphs)
 
-print("Number of glyphs: " .. pgs.num_glyphs)
-
-for i = 0, pgs.num_glyphs - 1 do
-    local glyph_info = pgs.glyphs[i]
-    print(string.format("Glyph %d: index=%d, x_advance=%d, y_advance=%d", i, glyph_info.glyph, glyph_info.geometry.x_advance, glyph_info.geometry.y_advance))
+-- Results
+print("Number of glyphs:", glyphs.num_glyphs)
+for i = 0, glyphs.num_glyphs - 1 do
+  local g = glyphs.glyphs[i]
+  print(string.format("Glyph %d: index=%d, x_advance=%d", i, g.glyph, g.geometry.x_advance))
 end
